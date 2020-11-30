28f4a9f1572ef48fc89612f888687b05283e8caf
zhenke.duan <zhenke.duan@i-tudou.com>
2020-11-30 18:23:33 +0800

    后端发短信修改

--- guanxiuChat/src/main/java/com/guan/controller/SendMsgController.java
+++ guanxiuChat/src/main/java/com/guan/controller/SendMsgController.java
@@ -1,13 +1,13 @@
 package com.guan.controller;
 
+import com.guan.domain.dto.APIResponse;
 import com.guan.service.SendMsgService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -15,7 +15,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-@Controller
+@RestController
 @CrossOrigin
 public class SendMsgController {
     @Autowired
@@ -24,26 +24,35 @@ public class SendMsgController {
     private RedisTemplate<String, String> redisTemplate;
 
     /**
-     *
      * @param phoneNumber
      * @return
      */
     @GetMapping("sendMsg")
-    public String sendMsg(String phoneNumber) {
+    public APIResponse<Map<String, String>> sendMsg(String phoneNumber) {
         String code = redisTemplate.opsForValue().get(phoneNumber);
+        Map<String, String> res = new HashMap<>();
         //调用发送方法
         if (!StringUtils.isEmpty(code)) {
-            return phoneNumber + ":" + code + "已存在，没过期";
+            res.put("phoneNumber", phoneNumber);
+            res.put("code", code);
+            return APIResponse.success(res);
         }
         //生成验证码，并存储到redis中
         code = UUID.randomUUID().toString().substring(0, 4);
+        //TODO
+        code = "6666";
         HashMap<String, Object> map = new HashMap<>();
         map.put("code", code);
-        boolean isSend = sendMsgService.sendMsg(phoneNumber, map);
+//        boolean isSend = sendMsgService.sendMsg(phoneNumber, map);
+        //todo
+        boolean  isSend = true;
         if (isSend) {
             redisTemplate.opsForValue().set(phoneNumber, code, 5, TimeUnit.MINUTES);
-            return phoneNumber + ":" + code + "发送成功";
+            res.put("phoneNumber", phoneNumber);
+            res.put("code", code);
+            return APIResponse.success(res);
         }
-        return "短信验证码，发送失败：";
+        res.put(phoneNumber, "发送失败");
+        return APIResponse.error(777, "发送失败", res);
     }
 }
